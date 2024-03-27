aad79a67cede8bb115448a8a10dd27d4c84a0b91
Paul Ramon <peramon98.18@gmail.com>
2024-03-26 23:14:32 -0500

    Training typescript

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+let name = 'Paul';
+const last_name = 'Ramon';
+let hpPoints: number | 'FULL' = 10;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({name, last_name, hpPoints, isAlive});
+
+export{};
